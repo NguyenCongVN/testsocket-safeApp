c1f6c117dc9a9acc07e268d7dfc79a3b55a7d3b5
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2021-06-26 19:48:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,84 +5,130 @@ import * as io from "socket.io-client";
 function App() {
   const [token, setToken] = useState("");
   const [inputToken, setInputToken] = useState("");
-  var socket = useRef(null) 
+  const [inputUrl, setInputUrl] = useState("");
+  const [url, setUrl] = useState("");
+  var socket = useRef(null);
+  const [maDongBoLichSuHienTai, setMaDongBo] = useState(null);
   useEffect(() => {
-    if(token !== '')
-    {
-      socket.current = io("https://murmuring-beyond-51639.herokuapp.com", {
-      auth: {
-        token: token,
-      },
-    });
+    if (token !== "") {
+      socket.current = io("http://localhost:4000", {
+        auth: {
+          token: token,
+        },
+      });
       socket.current.on("connect", () => {
         console.log("Kết nối thành công");
       });
-  
+
       socket.current.on("connect_error", (err) => {
         console.log(err.message);
       });
-  
+
       socket.current.on("disconnect", () => {
         console.log("Mất kết nối với server");
       });
-  
+
       socket.current.on("thongBao", (data) => {
         console.log(data);
       });
 
       socket.current.on("dongBoThongTinTruyCapThanhCong", (data) => {
-        console.log('Thành công')
+        console.log("Thành công");
         console.log(data);
       });
 
-      socket.current.on('taoThongTinTruyCapLoi' , data => {
-        console.log(data)
-      })
-      socket.current.on('taoThongTinTruyCapThanhCong' , data => {
-        console.log(data)
-      })
+      socket.current.on("taoThongTinTruyCapLoi", (data) => {
+        console.log(data);
+      });
+      socket.current.on("taoThongTinTruyCapThanhCong", (data) => {
+        console.log(data);
+      });
 
       socket.current.on("dongBoThongTinTruyCapLoi", (data) => {
-        console.log('Có lỗi xảy ra')
+        console.log("Có lỗi xảy ra");
         console.log(data);
       });
 
       socket.current.on("thongTinTruyCap", (data) => {
-        console.log(data)
-        socket.current.emit('yeuCauXacNhanDongBoLichSuTruyCap')
-      })
+        console.log(data);
+        socket.current.emit("yeuCauXacNhanDongBoLichSuTruyCap");
+      });
 
       socket.current.on("dongBoLichSuLoi", (data) => {
-        console.log(data)
-      })
+        console.log(data);
+      });
 
       socket.current.on("xacThucDongBoLichSuLoi", (data) => {
-        console.log(data)
-      })
+        console.log(data);
+      });
 
       socket.current.on("xacThucDongBoLichSuThanhCong", (data) => {
-        console.log(data)
-      })
-      
+        console.log("Ma dong bo : " + data.toString());
+        setMaDongBo(data);
+      });
+
+      socket.current.on("yeuCauCapNhatLichSu", (data) => {
+        console.log(data);
+        socket.current.emit("capNhatLichSuThanhCong");
+      });
       console.log("creating event");
-      // socket.current.emit("taoThongTinTruyCap", {
-      //   url: "gooogle.com",
-      //   tinhTrang: "DaChan",
-      // });
-      socket.current.emit('dongBoLichSuTruyCap')
     }
-    
   }, [token]);
   return (
     <div className="App">
       <div>
-        <input onChange={(e) => {
-          console.log(e.target.value)
-          setInputToken(e.target.value)
-        }} />
-        <button onClick={() =>{
-          setToken(inputToken)
-        }}> Set token </button> 
+        <input
+          onChange={(e) => {
+            console.log(e.target.value);
+            setInputToken(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            setToken(inputToken);
+          }}
+        >
+          {" "}
+          Set token{" "}
+        </button>
+      </div>
+      <div>
+        <input
+          placeholder="Nhập vào url"
+          onChange={(e) => {
+            setInputUrl(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            setUrl(inputUrl);
+            console.log(inputUrl);
+            socket.current.emit("taoThongTinTruyCap", {
+              url: inputUrl,
+              tinhTrang: "DaChan",
+            });
+          }}
+        >
+          Truy Cập
+        </button>
+        <br />
+        <button
+          onClick={() => {
+            if (maDongBoLichSuHienTai) {
+              console.log("Ma dong bo : " + maDongBoLichSuHienTai.toString());
+            }
+            if (!maDongBoLichSuHienTai) {
+              socket.current.emit("dongBoLichSuTruyCap");
+            } else {
+              socket.current.emit("dongBoLichSuTruyCap", {
+                maThongTinDongBoLichSu: maDongBoLichSuHienTai,
+              });
+            }
+          }}
+        >
+          {" "}
+          Đồng bộ lịch sử truy cập{" "}
+        </button>
       </div>
     </div>
   );
